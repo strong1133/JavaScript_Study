3910e27c2fca389df7aae46f42ccb243e4f8fe1b
strong1133 <strong1133@naver.com>
2022-02-06 16:12:30 +0900

    jest 예제

---
+++ node_test/cpt01/index.js
@@ -0,0 +1,23 @@
+class Site {
+    constructor() {
+        this.boards = [];
+    }
+
+    addBoard(board) {
+        this.boards.push(board);
+    }
+}
+class Board {
+    
+}
+
+class Article {}
+
+class Comment {}
+
+module.exports = {
+    Site,
+    Board,
+    Article,
+    Comment,
+};
