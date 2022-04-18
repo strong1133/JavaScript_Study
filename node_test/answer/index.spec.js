69cf055a9bda7dcf4176ea974609645d0a516464
strong1133 <strong1133@naver.com>
2022-04-18 09:26:52 +0900

    node jest tdd set up

---
+++ node_test/answer/index.spec.js
@@ -0,0 +1,8 @@
+describe('Jest Test', () => {
+
+    test('Jest Test', () => {
+        expect(() => {
+            let a =1
+        }).not.toThrow();
+    });
+});
--- node_test/cpt01/search.js
+++
@@ -1,8 +0,0 @@
-
-let boards =[];
-
-boards.push('한글말');
-boards.push('영어');
-let temp =boards.find((board)=> board=='한글말');
-
-console.log(temp)
