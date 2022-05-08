24ee0e5d37a71715717c22848e2d0838e51ea15e
strong1133 <strong1133@naver.com>
2022-05-08 21:23:23 +0900

    morgan 미들웨어 등록

--- node_test/inflearn/tdd/class02/app.js
+++ node_test/inflearn/tdd/class02/app.js
@@ -1,20 +1,26 @@
 const express = require('express');
 const app = express();
+const router = express.Router();
+const morgan = require("morgan");
+
 const port = 3000;
 
-const router = express.Router();
+let users = [
+    {id:1,name:"정석진"},
+    {id:2,name:"정형진"},
+    {id:3,name:"홍슬기"}
+];
 
-const logger = (req, res, next) =>{
-    console.log("LOG :: ");
-    next();
-}
-
-app.use(logger);
+app.use(morgan('dev')); // logger 
 app.use(router);
 
 
 router.get("/", (req, res)=>{
-    res.json("Test Get")
+    res.json("Test Get");
+});
+
+router.get("/users", (req, res)=>{
+    res.json(users);
 })
 
 app.listen(port, ()=>{
