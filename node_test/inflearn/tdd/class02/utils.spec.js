e60cbf53293462872f5b53361dd9cc72cd108ef8
strong1133 <strong1133@naver.com>
2022-05-08 23:43:04 +0900

    should

--- node_test/inflearn/tdd/class02/utils.spec.js
+++ node_test/inflearn/tdd/class02/utils.spec.js
@@ -1,11 +1,12 @@
 const utils = require('./utils')
-const asert = require('assert');
+const should = require('should');
 
 describe('utils.js 모듈의 Capitalize 함수는 ', ()=>{
     it('문자열의 첫번째 문자를 대문자로 변환한다.',() =>{
 
         const result = utils.capitalize('hello');
-        asert.equal(result,'Hello');
+        // asert.equal(result,'Hello');
+        result.should.be.equal('Hello');
     });
 });
 
