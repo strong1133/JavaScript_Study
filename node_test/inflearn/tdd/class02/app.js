cffbcef3e16971df852504f048b661e1a0ac8218
strong1133 <strong1133@naver.com>
2022-05-11 00:25:47 +0900

    get users super test >> 성공2, 실패1 test

--- node_test/inflearn/tdd/class02/app.js
+++ node_test/inflearn/tdd/class02/app.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const router = express.Router();
 const morgan = require("morgan");
@@ -6,25 +6,28 @@ const morgan = require("morgan");
 const port = 3000;
 
 let users = [
-    {id:1,name:"정석진"},
-    {id:2,name:"정형진"},
-    {id:3,name:"홍슬기"}
+    { id: 1, name: "정석진" },
+    { id: 2, name: "정형진" },
+    { id: 3, name: "홍슬기" },
 ];
 
-app.use(morgan('dev')); // logger 
+app.use(morgan("dev")); // logger
 app.use(router);
 
-
-app.get("/", (req, res)=>{
+app.get("/", (req, res) => {
     res.json("Test Get");
 });
 
-app.get("/users", (req, res)=>{
-    res.json(users);
-})
+app.get("/users", (req, res) => {
+    const limit = req.query.limit ? parseInt(req.query.limit, 10) : 10;
+    if (Number.isNaN(limit)) {
+        return res.status(400).end();
+    }
+    res.json(users.slice(0, limit));
+});
 
-app.listen(port, ()=>{
-    console.log('Express Is Run :: ', port);
-})
+app.listen(port, () => {
+    console.log("Express Is Run :: ", port);
+});
 
 module.exports = app;
--- node_test/inflearn/tdd/class02/index.spec.js
+++ node_test/inflearn/tdd/class02/index.spec.js
@@ -3,13 +3,35 @@ const should = require("should");
 
 const app = require("./app");
 
-describe("GET /user", () => {
-    it("res with JSON", (done) => {
-        request(app)
-        .get('/users')
-        .end((err,res)=>{
-            console.log(res.body);
-            done();
+describe("GET /user 는", () => {
+    describe("성공시", () => {
+        it("유저 객체를 담은 배열을 응답한다.", (done) => {
+            request(app)
+                .get("/users")
+                .end((err, res) => {
+                    res.body.should.be.instanceOf(Array); // res.body의 타입이 Array인지 검증.
+                    done();
+                });
+        });
+
+        it("최대 limit 만큼 응답한다.", (done) => {
+            request(app)
+                .get("/users?limit=2")
+                .end((err, res) => {
+                    res.body.should.have.lengthOf(2);
+                    done();
+                });
+        });
+    });
+
+    describe("실패시", () => {
+        it("limit이 숫자가 아니면 400을 응답한다.", (done) => {
+            request(app)
+                .get("/users?limit=two") 
+                .expect(400)
+                .end((err, res) =>{
+                    done();
+                });
         });
     });
 });
